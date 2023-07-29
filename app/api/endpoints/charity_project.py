b8d98587f621398398a5e53090c589bb850c9e58
petrovi4 <40.10.444@gmail.com>
2023-07-29 15:41:16 +0300

    Version for review 1.1

--- app/api/endpoints/charity_project.py
+++ app/api/endpoints/charity_project.py
@@ -6,56 +6,62 @@ from app.core.db import get_async_session
 from app.core.user import current_superuser
 from app.crud.charity_project import charity_project_crud
 from app.schemas.charity_project import (
-    CharityProjectCreate, CharityProjectDB, CharityProjectUpdate
+    CharityProjectCreate,
+    CharityProjectDB,
+    CharityProjectUpdate,
 )
 from app.services.investment import calculate_investment
 from .validators import (
-    check_project_before_edit, check_project_before_deletion, check_name_duplicate
+    check_project_before_edit,
+    check_project_before_deletion,
+    check_name_duplicate,
 )
 
 router = APIRouter()
 
 
 @router.get(
-    '/',
+    "/",
     response_model=List[CharityProjectDB],
     response_model_exclude_none=True,
 )
 async def get_all_charity_projects(
-        session: AsyncSession = Depends(get_async_session),
+    session: AsyncSession = Depends(get_async_session),
 ):
     """Получает список всех проектов."""
     return await charity_project_crud.get_all(session)
 
 
 @router.post(
-    '/',
+    "/",
     response_model=CharityProjectDB,
     response_model_exclude_none=True,
     dependencies=[Depends(current_superuser)],
 )
 async def create_charity_project(
-        charity_project: CharityProjectCreate,
-        session: AsyncSession = Depends(get_async_session),
+    charity_project: CharityProjectCreate,
+    session: AsyncSession = Depends(get_async_session),
 ):
     """
     Только для суперюзеров.
     Создает благотворительный проект.
     """
     await check_name_duplicate(charity_project.name, session)
-    new_charity_project = await charity_project_crud.create(charity_project, session)
+    new_charity_project = await charity_project_crud.create(
+        charity_project, session
+    )
     await calculate_investment(new_charity_project, session)
     return new_charity_project
 
 
 @router.delete(
-    '/{project_id}',
+    "/{project_id}",
     response_model=CharityProjectDB,
     dependencies=[Depends(current_superuser)],
 )
 async def delete_charity_project(
-        project_id: int,
-        session: AsyncSession = Depends(get_async_session),
+    project_id: int,
+    session: AsyncSession = Depends(get_async_session),
 ):
     """
     Только для суперюзеров.
@@ -63,19 +69,21 @@ async def delete_charity_project(
     в который уже были инвестированы средства, его можно только закрыть.
     """
     charity_project = await check_project_before_deletion(project_id, session)
-    charity_project = await charity_project_crud.remove(charity_project, session)
+    charity_project = await charity_project_crud.remove(
+        charity_project, session
+    )
     return charity_project
 
 
 @router.patch(
-    '/{project_id}',
+    "/{project_id}",
     response_model=CharityProjectDB,
     dependencies=[Depends(current_superuser)],
 )
 async def update_charity_project(
-        project_id: int,
-        obj_in: CharityProjectUpdate,
-        session: AsyncSession = Depends(get_async_session),
+    project_id: int,
+    obj_in: CharityProjectUpdate,
+    session: AsyncSession = Depends(get_async_session),
 ):
     """
     Только для суперюзеров.
